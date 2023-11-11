7aaabca24d7a165ff52500a9f8b9c12274f49647
gvrDev <guilhermevrodrigues123@gmail.com>
2023-11-11 18:07:24 -0300

    initial movement and collision

--- src/main.js
+++ src/main.js
@@ -1,21 +1,49 @@
 import Phaser from "phaser";
 
+class Player extends Phaser.Physics.Arcade.Image {
+  constructor(scene, x, y, texture) {
+    super(scene, x, y, texture);
+    scene.physics.world.enableBody(this);
+    this.setPushable(false);
+    this.setCollideWorldBounds(true);
+
+    this.moveSpeed = 200;
+  }
+}
+
 class Game extends Phaser.Scene {
   constructor() {
     super({ key: "Game" });
   }
 
-  preload() {}
+  preload() {
+    this.load.image("player", "player.png");
+  }
 
   create() {
-    let hello_text = this.add.text(
-      this.sys.game.config.width / 2,
-      this.sys.game.config.height / 2,
-      "Hello world!",
-      { fontSize: "64px", color: "#ffffff" },
+    this.player = new Player(this, 200, 200, "player");
+    this.secondPlayer = new Player(this, 64, 64, "player");
+    this.add.existing(this.player);
+    this.add.existing(this.secondPlayer);
+
+    this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+  }
+
+  update() {
+    let localVelocity = new Phaser.Math.Vector2(
+      this.keyD.isDown - this.keyA.isDown,
+      this.keyS.isDown - this.keyW.isDown,
+    ).normalize();
+
+    this.player.setVelocity(
+      localVelocity.x * this.player.moveSpeed,
+      localVelocity.y * this.player.moveSpeed,
     );
 
-    hello_text.setOrigin(0.5, 0.5);
+    this.physics.collide(this.player, this.secondPlayer);
   }
 }
 
@@ -30,6 +58,7 @@ const gameConfig = {
   },
   backgroundColor: "#5c5b5b",
   scene: [Game],
+  physics: { default: "arcade" },
 };
 
 const game = new Phaser.Game(gameConfig);
